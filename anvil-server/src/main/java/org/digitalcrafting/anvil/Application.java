4140ee8e58ea31f527c175f1fb36427cd008ed78
DigitalCrafting <k.kasprzyk2@gmail.com>
2023-06-25 12:43:31 +0200

    Initial pom.xml and module

---
+++ anvil-server/src/main/java/org/digitalcrafting/anvil/Application.java
@@ -0,0 +1,7 @@
+package org.digitalcrafting.anvil;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Working!");
+    }
+}
---
+++ anvil-server/src/test/java/org/digitalcrafting/anvil/ApplicationTest.java
@@ -0,0 +1,12 @@
+package org.digitalcrafting.anvil;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
